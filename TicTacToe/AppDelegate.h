105f3647cd7b0d61b848900d1cd71a59f8b29b4a
Andrew Nguyen <anguy080@ucr.edu>
2015-07-16 13:11:55 -0700

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Andrew  Nguyen on 7/16/15.
+//  Copyright (c) 2015 Andrew Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Andrew  Nguyen on 7/16/15.
+//  Copyright (c) 2015 Andrew Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
